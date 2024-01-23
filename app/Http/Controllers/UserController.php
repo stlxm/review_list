c7b1539b9ff80dd3990ed9922c212fcc8ee5cbc4
stlxm <meito0508@gmail.com>
2024-01-23 15:11:10 +0900

    送信先を作る

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,19 @@ class UserController extends Controller
    {
        return view('register');
    }
+
+   public function register(Request $request)
+   {
+       $user = User::query()->create([
+           'name'=>$request['name'],
+           'email'=>$request['email'],
+           'password'=>Hash::make($request['password'])
+       ]);
+
+       Auth::login($user);
+
+       return redirect()->route('profile');
+   }
 }
+
+
